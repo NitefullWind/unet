ee727726f6ad66061f0829adc80a746a73448e03
NitefullWind <nitefullwind@163.com>
2020-08-28 16:32:59 +0800

    修复Writen函数写大文件数据错误问题；修改socket操作errror报错信息

--- Demo/tinyserver/tinyserver/sockets.cpp
+++ Demo/tinyserver/tinyserver/sockets.cpp
@@ -52,7 +52,7 @@ int sockets::Accept(int sockfd, struct sockaddr_in *addr)
 #endif
 	if(connfd < 0) {
 		int savedError = errno;
-		TLOG_FATAL("accept error");
+		TLOG_ERROR("accept error [errno = " << errno << ", error string: " << strerror(errno) << "]");
 		switch(savedError)
 		{
 			case EAGAIN:
@@ -72,10 +72,10 @@ int sockets::Accept(int sockfd, struct sockaddr_in *addr)
 			case ENOTSOCK:
 			case EOPNOTSUPP:
 				// unexpected errors
-				TLOG_FATAL("unexpected error of ::accept " << savedError);
+				TLOG_ERROR("unexpected error of ::accept " << savedError);
 				break;
 			default:
-				TLOG_FATAL("unknown error of ::accept " << savedError);
+				TLOG_ERROR("unknown error of ::accept " << savedError);
 				break;
 		}
 	}
@@ -85,7 +85,7 @@ int sockets::Accept(int sockfd, struct sockaddr_in *addr)
 void sockets::Bind(int sockfd, const struct sockaddr_in& addr)
 {
 	if(::bind(sockfd, SockaddrCast(&addr), sizeof(addr)) < 0) {
-		TLOG_FATAL("bind error");
+		TLOG_FATAL("bind error [errno = " << errno << ", error string: " << strerror(errno) << "]");
 		abort();
 	}
 }
@@ -93,7 +93,7 @@ void sockets::Bind(int sockfd, const struct sockaddr_in& addr)
 void sockets::Close(int sockfd)
 {
 	if(::close(sockfd) < 0) {
-		TLOG_FATAL("close error");
+		TLOG_ERROR("close error [errno = " << errno << ", error string: " << strerror(errno) << "]");
 	}
 }
 
@@ -108,13 +108,13 @@ int sockets::CreateNonblockingSocket()
 #if VALGRIND
 	int sockfd = Socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if(sockfd < 0) {
-		TLOG_FATAL(__FUNCTION__ << " failed");
+		TLOG_FATAL(__FUNCTION__ << " failed [errno = " << errno << ", error string: " << strerror(errno) << "]");
 	}
 	SetNonBlockAndCloseOnExec(sockfd);
 #else
 	int sockfd = Socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, IPPROTO_TCP);
 	if(sockfd < 0) {
-		TLOG_FATAL(__FUNCTION__ << " failed");
+		TLOG_FATAL(__FUNCTION__ << " failed [errno = " << errno << ", error string: " << strerror(errno) << "]");
 	}
 #endif
 	return sockfd;
@@ -124,10 +124,10 @@ const char *sockets::Inet_ntop(int family, const void *addrptr, char *strptr, si
 {
 	const char *ptr;
 	if(strptr == nullptr) {
-		TLOG_FATAL("NULL 3rd argument to inet_ntop");
+		TLOG_ERROR("NULL 3rd argument to inet_ntop [errno = " << errno << ", error string: " << strerror(errno) << "]");
 	}
 	if((ptr = ::inet_ntop(family, addrptr, strptr, static_cast<socklen_t>(len))) == nullptr) {
-		TLOG_FATAL("inet_ntop error");
+		TLOG_ERROR("inet_ntop error [errno = " << errno << ", error string: " << strerror(errno) << "]");
 	}
 	return ptr;
 }
@@ -135,14 +135,14 @@ const char *sockets::Inet_ntop(int family, const void *addrptr, char *strptr, si
 void sockets::Inet_pton(int family, const char *strptr, void *addrptr)
 {
 	if(::inet_pton(family, strptr, addrptr) <= 0) {
-		TLOG_FATAL("inet_pton error for " << strptr);
+		TLOG_ERROR("inet_pton error for " << strptr << " [errno = " << errno << ", error string: " << strerror(errno) << "]");
 	}
 }
 
 void sockets::Listen(int sockfd)
 {
 	if(::listen(sockfd, SOMAXCONN) < 0) {
-		TLOG_FATAL("listen error");
+		TLOG_FATAL("listen error [errno = " << errno << ", error string: " << strerror(errno) << "]");
 	}
 }
 
@@ -150,7 +150,7 @@ int sockets::Poll(struct pollfd *fds, unsigned long nfds, int timeout)
 {
 	int n;
 	if((n = ::poll(fds, nfds, timeout)) < 0) {
-		TLOG_FATAL("poll error");
+		TLOG_FATAL("poll error [errno = " << errno << ", error string: " << strerror(errno) << "]");
 	}
 	return n;
 }
@@ -159,7 +159,7 @@ ssize_t sockets::Read(int fd, void *buf, size_t nbytes)
 {
 	ssize_t n;
 	if((n = ::read(fd, buf, nbytes)) == -1) {
-		TLOG_FATAL("read error");
+		TLOG_ERROR("read error [errno = " << errno << ", error string: " << strerror(errno) << "]");
 	}
 	return n;
 }
@@ -168,30 +168,33 @@ ssize_t sockets::Write(int fd, const void *buf, size_t nbytes)
 {
 	ssize_t n;
 	if((n = ::write(fd, buf, nbytes)) != (ssize_t)nbytes) {
-		TLOG_FATAL("write error");
+		TLOG_ERROR("write error [errno = " << errno << ", error string: " << strerror(errno) << "]");
 	}
 	return n;
 }
 
 ssize_t sockets::Writen(int fd, const void *buf, size_t nbytes)
 {
-	ssize_t nwritten = 0;
-	while((size_t)nwritten < nbytes) {
-		ssize_t n = ::write(fd, buf, nbytes);
+	size_t nleft = nbytes;
+	const char *ptr = (const char*)buf;
+
+	while(nleft > 0) {
+		ssize_t n = ::write(fd, ptr, nleft);
 		if(n > 0) {
-			nwritten += n;
-		} else if (n < 0 && errno != EINTR) {		// ignore error(s): EINTR
-			TLOG_FATAL("write error");
+			nleft -= n;
+			ptr += n;
+		} else if (n < 0 && errno != EINTR && errno != EAGAIN) {		// ignore error(s): EINTR
+			TLOG_ERROR("writen error [errno = " << errno << ", error string: " << strerror(errno) << "]");
 			return -1;
 		}
 	}
-	return nwritten;
+	return nbytes;
 }
 
 void sockets::ShutdownWrite(int sockfd)
 {
 	if(::shutdown(sockfd, SHUT_WR) < 0) {
-		TLOG_FATAL("shudown SHUT_WR error");
+		TLOG_ERROR("shudown SHUT_WR error [errno = " << errno << ", error string: " << strerror(errno) << "]");
 	}
 }
 
@@ -199,7 +202,7 @@ int sockets::Socket(int family, int type, int protocol)
 {
 	int n;
 	if((n = ::socket(family, type, protocol)) < 0) {
-		TLOG_FATAL("socket error")
+		TLOG_FATAL("socket error [errno = " << errno << ", error string: " << strerror(errno) << "]")
 	}
 	return n;
 }
@@ -209,7 +212,7 @@ void sockets::FromHostPort(const char *ip, uint16_t port, struct sockaddr_in *ad
 	addr->sin_family = AF_INET;
 	addr->sin_port = sockets::HostToNetwork16(port);
 	if(::inet_pton(AF_INET, ip, &addr->sin_addr) <= 0) {
-		TLOG_FATAL("inet_pton in FromHostPort error");
+		TLOG_ERROR("inet_pton in FromHostPort error [errno = " << errno << ", error string: " << strerror(errno) << "]");
 	}
 }
 
@@ -227,7 +230,7 @@ sockaddr_in sockets::GetLocalAddr(int sockfd)
 	socklen_t addrlen = sizeof(localAddr);
 	bzero(&localAddr, addrlen);
 	if(::getsockname(sockfd, SockaddrCast(&localAddr), &addrlen) < 0) {
-		TLOG_FATAL("getsockname error");
+		TLOG_ERROR("getsockname error [errno = " << errno << ", error string: " << strerror(errno) << "]");
 	}
 	return localAddr;
 }
@@ -238,7 +241,7 @@ sockaddr_in sockets::GetPeerAddr(int sockfd)
 	socklen_t addrlen = sizeof(peerAddr);
 	bzero(&peerAddr, addrlen);
 	if(::getpeername(sockfd, SockaddrCast(&peerAddr), &addrlen) < 0) {
-		TLOG_FATAL("getpeername error");
+		TLOG_ERROR("getpeername error [errno = " << errno << ", error string: " << strerror(errno) << "]");
 	}
 	return peerAddr;
 }
